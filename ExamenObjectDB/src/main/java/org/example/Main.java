7d760f43312135149ec87500b90f066ab43902da
CarlosCastroFernandez <bubachico@gmail.com>
2024-03-04 13:33:39 +0100

    Se elimina Try Catch
    Se pone Generated value y se elimina try catch

--- ExamenObjectDB/src/main/java/org/example/Main.java
+++ ExamenObjectDB/src/main/java/org/example/Main.java
@@ -7,22 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         ClienteDAO dao=new ClienteDAO();
-        Cliente cliente1=new Cliente(1L,"Carlos",20L,"activo");
-        Cliente cliente2=new Cliente(2L,"Naomi",34L,"activo");
-        Cliente cliente3=new Cliente(3L,"Pablo",10L,"inactivo");
-        Cliente cliente4=new Cliente(4L,"Raul",54L,"inactivo");
-        Cliente cliente5=new Cliente(5L,"Pepe",44L,"activo");
+        Cliente cliente1=new Cliente("Carlos",20L,"activo");
+        Cliente cliente2=new Cliente("Naomi",34L,"activo");
+        Cliente cliente3=new Cliente("Pablo",10L,"inactivo");
+        Cliente cliente4=new Cliente("Raul",54L,"inactivo");
+        Cliente cliente5=new Cliente("Pepe",44L,"activo");
         //Insertar Cliente
-        try{
             dao.insertarCliente(cliente2);
             dao.insertarCliente(cliente3);
             dao.insertarCliente(cliente4);
             dao.insertarCliente(cliente5);
             dao.insertarCliente(cliente1);
-        }catch(Exception e){
 
-        }
-        
+
+
         //Informacion de un cliente por ID
         dao.getCliente(2L);
 
--- ExamenObjectDB/src/main/java/org/example/clase/Cliente.java
+++ ExamenObjectDB/src/main/java/org/example/clase/Cliente.java
@@ -1,12 +1,15 @@
 package org.example.clase;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 @Entity
 
 public class Cliente implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private Long totalVentas;
