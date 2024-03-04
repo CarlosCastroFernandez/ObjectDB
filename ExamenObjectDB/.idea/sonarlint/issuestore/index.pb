b81c4addfda5ff997b269b33a7753fbdd699d74d
CarlosCastroFernandez <bubachico@gmail.com>
2024-03-04 13:21:39 +0100

    Actualiza Log
    Se pone Logger de manera correcta

---
+++ ExamenObjectDB/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+a
+1src/main/java/org/example/utils/ObjectDBUtil.java,e\0\e0333cdd58fb348ac62330b78b518c50c5fe35ad
+\
+,src/main/java/org/example/clase/Cliente.java,d\e\de392121067d908c29981c1ff2994bb728e7c8ff
+]
+-src/main/java/org/example/DAO/ClienteDAO.java,a\6\a6131dfe9f4cc22d8729a8b7b051c4506b49e2e0
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
--- ExamenObjectDB/src/main/java/org/example/utils/ObjectDBUtil.java
+++ ExamenObjectDB/src/main/java/org/example/utils/ObjectDBUtil.java
@@ -6,10 +6,13 @@ import java.util.logging.Logger;
 
 public class ObjectDBUtil {
     private static final EntityManagerFactory entityManagerFactory;
-    //private static Logger log=new Logger(ObjectDBUtil.class.getClassLoader().getName());
+    private static Logger log= Logger.getLogger(ObjectDBUtil.class.getName());
 
     static{
         entityManagerFactory= Persistence.createEntityManagerFactory("data.odb");
+        log.info("Conexion exitosa");
+
+
 
     }
     public static EntityManagerFactory getEntityManagerFactory(){
